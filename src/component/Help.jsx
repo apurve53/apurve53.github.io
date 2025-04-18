3405d74f6e5f621975aa96bb14503af3979b931a
Apurve Srivastava <apurve2014@gmail.com>
2025-04-18 20:09:42 +0530

    This is only cover page. for github pages

--- src/component/Help.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Help() {
-  function openChatBar() {
-
-  }
-  return (
-    <div onClick={() => { openChatBar() }} className="chat-bar">How can I help you</div>
-  )
-}
--- src/component/Open.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Open() {
-  return (
-    <div>Open</div>
-  )
-}
